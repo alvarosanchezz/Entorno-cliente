e14621bcc1c1ced28aaf8c78025252f6ed2eb672
Alvaro Sanchez <alvsanrod2006@gmail.com>
2025-09-17 12:11:41 +0200

    Primer commit

---
+++ BOLETIN I/ScriptEjercicio1.js
@@ -0,0 +1,8 @@
+function saludar(){
+     var edad = prompt("¿Cuál es tu edad?");
+     if(edad >= 18){
+        alert("Eres mayor de edad");
+     } else {
+        alert("Eres menor de edad");
+        }
+    }
---
+++ BOLETIN I/ScriptEjercicio2.js
@@ -0,0 +1,4 @@
+function pintar_ruta(){
+    var ruta = prompt("Introduce una ruta de archivo o URL");
+    document.write(ruta);
+}
